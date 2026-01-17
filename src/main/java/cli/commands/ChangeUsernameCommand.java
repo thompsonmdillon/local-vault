f417396bcc2332d79dc38611195ed2cf4fd0cb89
Thompson <tun78919@temple.edu>
2026-01-16 22:28:07 -0500

    Created structure for the changing username command and wrote code for it too. Not fully implemented but wrote comments for what still needs to be done.

--- src/main/java/cli/commands/ChangeUsernameCommand.java
+++ src/main/java/cli/commands/ChangeUsernameCommand.java
@@ -1,7 +1,39 @@
 package cli.commands;
+import java.util.Scanner;
 
 public class ChangeUsernameCommand {
+
+    /* STILL NEED TO IMPLEMENT:
+    1. Replace variables and values and check if conditional statements once I figure out how to store usernames
+    persistently across classes.
+    2. Store the new username persistently and replace the old one. */
+
     public static void changeUsername(){
-        System.out.println("Change username method successfully executed.");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        System.out.println("====================================================================================================================");
+        System.out.println();
+        System.out.println("Your current username is USERNAME HERE I KNOW HOW TO STORE PERSISTENTLY");
+        System.out.println();
+        System.out.println("====================================================================================================================");
+        System.out.println();
+        System.out.println(""" 
+                Type 8 or exit if you wish to keep your current username. Otherwise, please type what you would like to 
+                change your username to and press enter: """);
+        System.out.println();
+        System.out.println("====================================================================================================================");
+        String newUsername = scanner.nextLine();
+        if(newUsername.equals("8") || newUsername.equalsIgnoreCase("exit")){
+            System.out.println();
+            System.out.println("Username not changed. Returning to the command menu...");
+            System.out.println();
+        } else {
+            // Store the new username persistently and replace the old one.
+            System.out.println();
+            System.out.println("================================================================================================================");
+            System.out.println();
+            System.out.print("Your new username is PERSISTENT USERNAME HERE");
+            System.out.println();
+        }
     }
 }
