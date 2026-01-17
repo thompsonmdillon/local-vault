f6166e9466ab6ca8dc0016c08baa93c9d36eb9bb
Thompson <tun78919@temple.edu>
2026-01-16 22:05:59 -0500

    Wrote more code for methods in the Launch class. Included comments for what still needs to be done and implemented.

--- src/main/java/cli/Launch.java
+++ src/main/java/cli/Launch.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class Launch {
 
+    /* STILL NEED TO IMPLEMENT:
+    1. Persistently store username and password values for returning users. Also, add that to the functions when done.
+    Likely use JSON.
+    2. Write a check for if the LocalVault file exists, meaning, the program has already ran before on the user's
+    system. Like an executable. Do this in launch().
+    3. Once I figure out how to persistently store the username and password values for users, make sure to implement
+    those values into the functions and entirety of the Launch class. Specifically in the if conditionals throughout
+    the methods in this class.
+    4. Add/delete/edit global variables in this class. */
+
     // Global variable for username, change when user creates username.
     public static String username = "placeholder value";
     public static String password = "placeholder value";
@@ -11,29 +21,27 @@ public class Launch {
     public static boolean vaultFileExists = false;
 
     public static void launch() {
-        // may need a global boolean variable for checking if vaultFile exists.
-        boolean vaultFile = false;
-        // Define username once user has created their username.
-        String username;
-        // if it does not, then run first time setup.
-        if (vaultFile == false){
+        /* may need a global boolean variable for checking if vaultFile exists. Need to figure out how to check if the
+        vaultFile, that is created once the application, exists. */
+        // boolean vaultFileExists = false;
+        // if it does not exist, then run first time setup.
+        if (vaultFileExists == false){
             firstLaunchSetup();
         } else {
-            boolean correctPassword = false;
-            if (correctPassword = checkReturningUserPassword() == true){
-                return;
+            // if it does exist, then prompt user to enter password and have them enter it and check what they enter.
+            boolean incorrectPassword = true;
+            while (incorrectPassword){
+                if (checkReturningUserPassword() == true){
+                    incorrectPassword = false;
+                }
             }
-            // Otherwise if vault file exists prompt the user for their password.
-            Scanner scanner = new Scanner(System.in);
-            String masterPassword = scanner.nextLine();
-            // if masterPassword matches the masterKey, then launch the cli menu for LocalVault.
-            //if(masterPassword == )
         }
     }
 
+    // This is pretty much fully implemented. Just configure once figure out how to persistently store users' passwords.
     public static void firstLaunchSetup(){
         Scanner scanner = new Scanner(System.in);
-        String confirmPassword = "";
+        String confirmPassword;
         System.out.println("====================================================================================================================");
         System.out.println();
         System.out.println("Hello new user! Thank you for downloading the LocalVault application.");
@@ -41,28 +49,62 @@ public class Launch {
         System.out.println("====================================================================================================================");
         System.out.println();
         System.out.println("Please create and enter a username: ");
-        username = scanner.nextLine();
         System.out.println();
         System.out.println("====================================================================================================================");
+        username = scanner.nextLine();
         System.out.println();
         boolean passwordLoop = true;
         while (passwordLoop) {
+            System.out.println("================================================================================================================");
+            System.out.println();
             System.out.println("Please create and enter a password: ");
+            System.out.println();
+            System.out.println("================================================================================================================");
             password = scanner.nextLine();
+            System.out.println("================================================================================================================");
             System.out.println();
             System.out.println("Please confirm your password: ");
+            System.out.println();
+            System.out.println("================================================================================================================");
             confirmPassword = scanner.nextLine();
             if(confirmPassword.equals(password)){
                 System.out.println("Success. Local account activation completed.");
                 passwordLoop = false;
             } else {
+                System.out.println();
                 System.out.println("Passwords do not match. Please try again.");
+                System.out.println();
             }
         }
     }
 
+    /* Still need to figure out how to store the information (username and password) persistently, probably using
+    * JSON. */
     public static boolean checkReturningUserPassword(){
-        if
+        // Instead of user, print the persistently stored username to welcome the user back.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("====================================================================================================================");
+        System.out.println();
+        System.out.println("Welcome back user! Please enter your password to enter LocalVault:");
+        System.out.println();
+        System.out.println("====================================================================================================================");
+        String userPasswordAttempt = scanner.nextLine();
+        if (userPasswordAttempt.equals(password)){
+            System.out.println();
+            System.out.println("================================================================================================================");
+            System.out.println();
+            System.out.println("Success! Proceeding to user's LocalVault.");
+            System.out.println();
+            return true;
+        } else {
+            boolean passwordLoop = true;
+            while (passwordLoop){
+                System.out.println();
+                System.out.println("Incorrect password. Please try again.");
+                System.out.println();
+            }
+        }
         return true;
     }
+
 }
