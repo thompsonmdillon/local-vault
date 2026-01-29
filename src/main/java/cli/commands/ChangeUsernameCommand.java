ae34e976f6102ef4d22638fd6822664b501df309
Thompson <tun78919@temple.edu>
2026-01-29 14:45:42 -0500

    Added logic for the ChangeUsernameCommand class. Will need to change it once I can store a username persistently.

--- src/main/java/cli/commands/ChangeUsernameCommand.java
+++ src/main/java/cli/commands/ChangeUsernameCommand.java
@@ -1,4 +1,5 @@
 package cli.commands;
+import cli.CommandMenu;
 import java.util.Scanner;
 
 public class ChangeUsernameCommand {
@@ -6,35 +7,55 @@ public class ChangeUsernameCommand {
     /* STILL NEED TO IMPLEMENT:
     1. Replace variables and values and check if conditional statements once I figure out how to store usernames
     persistently across classes.
-    2. Store the new username persistently and replace the old one. */
+    2. Set current username equal to the new username once figured out how to store this data persistently.
+    3. Also, this does not need to be used to login, this is just for the system to address the local user in messages
+    and in the CLI.*/
 
-    public static void changeUsername(){
+    public static void changeUsername() {
+        String newUsername, newUsernameConfirm;
         Scanner scanner = new Scanner(System.in);
-        System.out.println();
         System.out.println("====================================================================================================================");
-        System.out.println();
-        System.out.println("Your current username is USERNAME HERE WHEN I KNOW HOW TO STORE PERSISTENTLY");
-        System.out.println();
+        System.out.println("Hello USERNAME.");
         System.out.println("====================================================================================================================");
-        System.out.println();
         System.out.println(""" 
-                Type and enter "8" or type and enter "exit" if you wish to keep your current username. Otherwise, please 
-                type what you would like to change your username to and press enter: """);
-        System.out.println();
+                Type and enter 'exit' if you wish to keep your current username. Please note that your username may not 
+                be 'exit' if you wish to use this application. Otherwise, please type and enter what you would like to 
+                change your password to and then press enter: """);
+        newUsername = scanner.next();
         System.out.println("====================================================================================================================");
-        String newUsername = scanner.nextLine();
-        if(newUsername.equals("8") || newUsername.equalsIgnoreCase("exit")){
-            System.out.println();
-            System.out.println("Username not changed. Returning to the command menu...");
-            System.out.println();
-        } else {
-            // Store the new username persistently and replace the old one.
-            System.out.println();
-            System.out.println("================================================================================================================");
-            System.out.println();
-            System.out.print("Your new username is PERSISTENT USERNAME HERE");
-            System.out.println();
+        if (newUsername.equalsIgnoreCase("exit")) {
+            System.out.println("Username not changed. Exiting the change username menu and returning to the command menu...");
+            CommandMenu.menu();
+            return;
         }
+        System.out.println("Please enter the same username as you entered above to confirm the change: ");
+        newUsernameConfirm = scanner.nextLine();
+        if (newUsernameConfirm.equalsIgnoreCase("exit")) {
+            System.out.println("Username not changed. Exiting the change username menu and returning to the command menu...");
+            CommandMenu.menu();
+            return;
+        }
+        while (!newUsername.equals(newUsernameConfirm)) {
+            System.out.println("The two usernames you entered did not match. Please try again.");
+            System.out.println("Type and enter what you would like to change your username to and then press enter: ");
+            newUsername = scanner.nextLine();
+            System.out.println("====================================================================================================================");
+            if (newUsername.equalsIgnoreCase("exit")) {
+                System.out.println("Username not changed. Exiting the change username menu and returning to the command menu...");
+                CommandMenu.menu();
+                return;
+            }
+            System.out.println("Please enter the same username as you entered above to confirm the change: ");
+            newUsernameConfirm = scanner.nextLine();
+            System.out.println("====================================================================================================================");
+            if (newUsernameConfirm.equalsIgnoreCase("exit")) {
+                System.out.println("Username not changed. Exiting the change username menu and returning to the command menu...");
+                CommandMenu.menu();
+                return;
+            }
+        }
+        //username = newUsername;
+        //return currentUsername;
     }
 
 }
